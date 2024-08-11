5d981150fbb54fc9f159d4b883f13b35b87037de
Jaehwan <nicecatellie@gmail.com>
2024-08-12 08:55:34 +0900

    aa

--- _03for/_01for.java
+++ _03for/_01for.java
@@ -13,19 +13,17 @@ public class _01for {
 //		} System.out.println(sum);
 
 		// 50이상의 평균을 구하시오
-		int sum=0;
-		int cnt=0;
-		int[] a= {30,40,50,55,60};
-		for (int i=0; i < 5; i++) {
-			if (a[i]>=50) {
-				sum += a[i];
-				cnt++;
-			}
-		}System.out.println((double)sum/cnt);
+//		int sum=0;
+//		int cnt=0;
+//		int[] a= {30,40,50,55,60};
+//		for (int i=0; i < 5; i++) {
+//			if (a[i]>=50) {
+//				sum += a[i];
+//				cnt++;
+//			}
+//		}System.out.println((double)sum/cnt);
+	
 		// 평균을 구하시오 단 최저 점수는 포함하지 마시오
-	
-	
-	
-	}
 
+	}
 }
